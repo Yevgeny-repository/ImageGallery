2ab5ea669ae038f96d4e255e7a5e8d07736a3c28
Yevgeny-repository <jenia_gor@yahoo.com>
2021-03-31 11:01:11 +0300

    Add files via upload

---
+++ ImageGallery.API/Profiles/ImageProfile.cs
@@ -0,0 +1,13 @@
+﻿using ImageGallery.Entities.Models;
+using ImageGallery.Shared.DTO;
+
+namespace ImageGallery.API.Profiles
+{
+    public class ImageProfile : AutoMapper.Profile
+    {
+        public ImageProfile()
+        {
+            CreateMap<Image, ImageResponse>();
+        }
+    }
+}
